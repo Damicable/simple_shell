85765816b5f9f0b9b5d2e48325b90e604ad4569c
sayed atef <sayedatef6293@gmail.com>
2023-05-24 23:18:29 +0000

    fixed helper_2.c file

--- helpers_2.c
+++ helpers_2.c
@@ -1,6 +1,99 @@
 #include "shell.h"
 
 /**
+ * logical_ops - This function checks a line for logical operators "||" or "&&".
+ * @line: The pointer to the character to check in the line.
+ * @new_len: Pointer to a new_len in get_new_len function.
+ */
+
+void logical_ops(char *line, ssize_t *new_len)
+{
+	char old, current, next;
+
+	old = *(line - 1);
+	current = *line;
+	next = *(line + 1);
+
+	if (current == '&')
+	{
+		if (next == '&' && old != ' ')
+			(*new_len)++;
+		else if (old == '&' && next != ' ')
+			(*new_len)++;
+	}
+	else if (current == '|')
+	{
+		if (next == '|' && old != ' ')
+			(*new_len)++;
+		else if (old == '|' && next != ' ')
+			(*new_len)++;
+	}
+}
+
+/**
+ * get_new_len -  This function, gets the new length of a line partitioned
+ * by ";", "||", "&&&", or "#".
+ * @line: The partitioned line to check.
+ *
+ * Return: New line length.
+ *
+ * Description: This cuts short lines containing '#' comments with '\0'.
+ */
+ssize_t get_new_len(char *line)
+{
+	size_t k;
+	ssize_t new_len = 0;
+	char current, next;
+
+	for (k = 0; line[k]; k++)
+	{
+		current = line[k];
+		next = line[k + 1];
+		if (current == '#')
+		{
+			if (k == 0 || line[k - 1] == ' ')
+			{
+				line[k] = '\0';
+				break;
+			}
+		}
+		else if (k != 0)
+		{
+			if (current == ';')
+			{
+				if (next == ';' && line[k - 1] != ' ' && line[k - 1] != ';')
+				{
+					new_len += 2;
+					continue;
+				}
+				else if (line[k - 1] == ';' && next != ' ')
+				{
+					new_len += 2;
+					continue;
+				}
+				if (line[k - 1] != ' ')
+					new_len++;
+				if (next != ' ')
+					new_len++;
+			}
+			else
+				logical_ops(&line[k], &new_len);
+		}
+		else if (current == ';')
+		{
+			if (k != 0 && line[k - 1] != ' ')
+				new_len++;
+			if (next != ' ' && next != ';')
+				new_len++;
+		}
+		new_len++;
+	}
+	return (new_len);
+}
+
+
+
+/**
  * handle_line - This function partitions a line read from standard input as
  * needed.
  * @line: The pointer to a line read from standard input.
@@ -90,95 +183,4 @@ void handle_line(char **line, ssize_t read)
 
 	free(*line);
 	*line = new_line;
-}
-
-/**
- * get_new_len -  This function, gets the new length of a line partitioned
- * by ";", "||", "&&&", or "#".
- * @line: The partitioned line to check.
- *
- * Return: New line length.
- *
- * Description: This cuts short lines containing '#' comments with '\0'.
- */
-
-ssize_t get_new_len(char *line)
-{
-	size_t k;
-	ssize_t new_len = 0;
-	char current, next;
-
-	for (k = 0; line[k]; k++)
-	{
-		current = line[k];
-		next = line[k + 1];
-		if (current == '#')
-		{
-			if (k == 0 || line[k - 1] == ' ')
-			{
-				line[k] = '\0';
-				break;
-			}
-		}
-		else if (k != 0)
-		{
-			if (current == ';')
-			{
-				if (next == ';' && line[k - 1] != ' ' && line[k - 1] != ';')
-				{
-					new_len += 2;
-					continue;
-				}
-				else if (line[k - 1] == ';' && next != ' ')
-				{
-					new_len += 2;
-					continue;
-				}
-				if (line[k - 1] != ' ')
-					new_len++;
-				if (next != ' ')
-					new_len++;
-			}
-			else
-				logical_ops(&line[k], &new_len);
-		}
-		else if (current == ';')
-		{
-			if (k != 0 && line[k - 1] != ' ')
-				new_len++;
-			if (next != ' ' && next != ';')
-				new_len++;
-		}
-		new_len++;
-	}
-	return (new_len);
-}
-/**
- * logical_ops - This function checks a line for logical operators "||" or "&&".
- * @line: The pointer to the character to check in the line.
- * @new_len: Pointer to a new_len in get_new_len function.
- */
-
-void logical_ops(char *line, ssize_t *new_len)
-{
-	char old, current, next;
-
-	old = *(line - 1);
-	current = *line;
-	next = *(line + 1);
-
-	if (current == '&')
-	{
-		if (next == '&' && old != ' ')
-			(*new_len)++;
-		else if (old == '&' && next != ' ')
-			(*new_len)++;
-	}
-	else if (current == '|')
-	{
-		if (next == '|' && old != ' ')
-			(*new_len)++;
-		else if (old == '|' && next != ' ')
-			(*new_len)++;
-	}
 }
